c0a24c00226e78adb201e42fb5029fb383d2f62c
Mansi Bhardwaj <mansibhardwaj2022@vitbhopal.ac.in>
2025-06-12 12:38:54 +0530

    leetcode:3 (Medium)

---
+++ Medium/Solution_3.java
@@ -0,0 +1,35 @@
+// Given a string s, find the length of the longest substring without duplicate characters.
+
+
+// Example 1:
+
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+// Example 2:
+
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+
+
+import java.util.HashMap;
+
+class Solution_3 {
+    public int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> charIndexMap = new HashMap<>();
+        int left = 0;
+        int maxLength = 0;
+        
+        for (int right = 0; right < s.length(); right++) {
+            char currentChar = s.charAt(right);
+            if (charIndexMap.containsKey(currentChar)) {
+                left = Math.max(left, charIndexMap.get(currentChar) + 1);
+            }
+            charIndexMap.put(currentChar, right);
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+        
+        return maxLength;
+    }
+}
