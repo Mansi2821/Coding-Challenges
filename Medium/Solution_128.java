458f2da08ed82983f50aad43bcabf6f3ce1e4047
Mansi Bhardwaj <mansibhardwaj2022@vitbhopal.ac.in>
2025-07-12 12:00:06 +0530

    Leetcode 128: Longest consecutive sequence (med)

---
+++ Medium/Solution_128.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+// Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+// You must write an algorithm that runs in O(n) time.
+
+ 
+
+// Example 1:
+
+// Input: nums = [100,4,200,1,3,2]
+// Output: 4
+// Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+
+class Solution_128 {
+  public int longestConsecutive(int[] nums) {
+    int ans = 0;
+    Set<Integer> seen = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+
+    for (int num : seen) {
+      // `num` is the start of a sequence.
+      if (seen.contains(num - 1))
+        continue;
+      int length = 1;
+      while (seen.contains(++num))
+        ++length;
+      ans = Math.max(ans, length);
+    }
+
+    return ans;
+  }
+}
--- README.md
+++ README.md
@@ -13,5 +13,5 @@ This repository contains my daily coding challenge solutions.
 | Difficulty | Problems Solved |
 |------------|-----------------|
 | Easy       | 17              |
-| Medium     | 11              |
+| Medium     | 12              |
 | Hard       | 04              |
