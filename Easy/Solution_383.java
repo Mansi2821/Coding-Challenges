85a7521a2b626ddc1a0004d2e1838dbe40760bbc
Mansi2821 <mansibhardwaj2022@vitbhopal.ac.in>
2025-09-03 14:05:26 +0530

    Leetcode 383: new sol- ransom note (easy)

--- Easy/Solution_383.java
+++ Easy/Solution_383.java
@@ -12,23 +12,42 @@
 
 import java.util.HashMap;
 
+// class Solution_383 {
+//     public boolean canConstruct(String ransomNote, String magazine) {
+//         HashMap<Character, Integer> magazineLetters = new HashMap<>();
+        
+//         // Count each letter in magazine
+//         for (char c : magazine.toCharArray()) {
+//             magazineLetters.put(c, magazineLetters.getOrDefault(c, 0) + 1);
+//         }
+        
+//         // Check if we can construct ransomNote from magazine
+//         for (char c : ransomNote.toCharArray()) {
+//             if (!magazineLetters.containsKey(c) || magazineLetters.get(c) <= 0) {
+//                 return false;
+//             }
+//             magazineLetters.put(c, magazineLetters.get(c) - 1);
+//         }
+        
+//         return true;
+//     }
+// }
+
+
+
 class Solution_383 {
-    public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character, Integer> magazineLetters = new HashMap<>();
-        
-        // Count each letter in magazine
-        for (char c : magazine.toCharArray()) {
-            magazineLetters.put(c, magazineLetters.getOrDefault(c, 0) + 1);
-        }
-        
-        // Check if we can construct ransomNote from magazine
-        for (char c : ransomNote.toCharArray()) {
-            if (!magazineLetters.containsKey(c) || magazineLetters.get(c) <= 0) {
-                return false;
-            }
-            magazineLetters.put(c, magazineLetters.get(c) - 1);
-        }
-        
-        return true;
+  public boolean canConstruct(String ransomNote, String magazine) {
+    int[] count = new int[26];
+
+    for (final char c : magazine.toCharArray())
+      ++count[c - 'a'];
+
+    for (final char c : ransomNote.toCharArray()) {
+      if (count[c - 'a'] == 0)
+        return false;
+      --count[c - 'a'];
     }
+
+    return true;
+  }
 }
