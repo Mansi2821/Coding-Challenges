dfc1f8f6f6dae321c8ef8e141776df869b1c3155
Mansi2821 <mansibhardwaj2022@vitbhopal.ac.in>
2025-09-07 23:17:07 +0530

    Leetcode 1304: N unique integers that sum up to 0 (easy)

---
+++ Easy/Solution_1304.java
@@ -0,0 +1,22 @@
+// Given an integer n, return any array containing n unique integers such that they add up to 0.
+
+// Example 1:
+
+// Input: n = 5
+// Output: [-7,-1,1,3,4]
+// Explanation: These arrays also are accepted [-5,-1,1,2,3] , [-3,-1,2,-2,4].
+// Example 2:
+
+// Input: n = 3
+// Output: [-1,0,1]
+
+class Solution_1304 {
+  public int[] sumZero(int n) {
+    int[] ans = new int[n];
+
+    for (int i = 0; i < n; ++i)
+      ans[i] = i * 2 - n + 1;
+
+    return ans;
+  }
+}
--- README.md
+++ README.md
@@ -12,6 +12,6 @@ This repository contains my daily coding challenge solutions.
 
 | Difficulty | Problems Solved |
 |------------|-----------------|
-| Easy       | 40              |
+| Easy       | 41              |
 | Medium     | 33              |
 | Hard       | 07              |
